3b3e635fa3bf2fc2109d232d344d84bf2ad7b331
StoryKing <xuanzhitao@xuanzhitaodeMacBook-Air.local>
2026-01-05 23:06:06 +0800

    feat: new cateogry

--- src/components/features/ComponentList/ComponentTable.tsx
+++ src/components/features/ComponentList/ComponentTable.tsx
@@ -56,23 +56,23 @@ export const ComponentTable = ({
               <TableRow key={i}>
                 <TableCell>
                   <div className="flex items-center gap-2">
-                    <Skeleton className="h-4 w-4" />
-                    <Skeleton className="h-4 w-32" />
+                    <Skeleton className="h-5 w-5" />
+                    <Skeleton className="h-5 w-32" />
                   </div>
                 </TableCell>
                 <TableCell>
-                  <Skeleton className="h-4 w-16" />
+                  <Skeleton className="h-5 w-20" />
                 </TableCell>
                 <TableCell>
                   <div className="flex justify-center">
-                    <Skeleton className="h-4 w-20" />
+                    <Skeleton className="h-5 w-20" />
                   </div>
                 </TableCell>
                 <TableCell className="hidden lg:table-cell">
-                  <Skeleton className="h-4 w-24" />
+                  <Skeleton className="h-5 w-24" />
                 </TableCell>
                 <TableCell>
-                  <Skeleton className="h-4 w-4" />
+                  <Skeleton className="h-8 w-8" />
                 </TableCell>
               </TableRow>
             ))}
